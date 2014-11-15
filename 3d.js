f6b161634481e2070392284cfb17a2242b7ab245
mattdesl <dave.des@gmail.com>
2014-11-15 16:57:56 -0500

    using modular vecmath

--- 3d.js
+++ 3d.js
@@ -1,3 +1,11 @@
-//will replace with more modular gl-vecN
-var vec3 = require('gl-matrix').vec3 
+var vec3 = {
+    create: require('gl-vec3/create'),
+    add: require('gl-vec3/add'),
+    multiply: require('gl-vec3/multiply'),
+    sub: require('gl-vec3/subtract'),
+    scale: require('gl-vec3/scale'),
+    copy: require('gl-vec3/copy'),
+    sqrLen: require('gl-vec3/squaredLength'),
+    fromValues: require('gl-vec3/fromValues'),
+}
 module.exports = require('./lib/build')(vec3)
